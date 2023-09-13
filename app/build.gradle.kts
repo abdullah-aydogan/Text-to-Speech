8ac1962307c3b70747a3147c395bad17ec17e140
Abdullah Aydoğan <aydoganabdullah34@gmail.com>
2023-09-13 16:50:27 +0300

    design fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "tr.abdullah.texttospeech"
         minSdk = 24
         targetSdk = 33
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
